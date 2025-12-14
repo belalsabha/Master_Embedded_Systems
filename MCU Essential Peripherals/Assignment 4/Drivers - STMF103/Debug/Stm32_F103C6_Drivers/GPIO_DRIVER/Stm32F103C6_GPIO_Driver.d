20174ec3577ebb3f2f501ce05bbcecfedf9180ec
Belal Hani Sabha <137720072+belalsabha@users.noreply.github.com>
2025-12-14 14:00:33 +0200

    Delete MCU Essential Peripherals/Assignment 4/Drivers - STMF103 directory

--- MCU Essential Peripherals/Assignment 4/Drivers - STMF103/Debug/Stm32_F103C6_Drivers/GPIO_DRIVER/Stm32F103C6_GPIO_Driver.d
+++
@@ -1,8 +0,0 @@
-Stm32_F103C6_Drivers/GPIO_DRIVER/Stm32F103C6_GPIO_Driver.o: \
- ../Stm32_F103C6_Drivers/GPIO_DRIVER/Stm32F103C6_GPIO_Driver.c \
- ../Stm32_F103C6_Drivers/GPIO_DRIVER/Stm32F103C6_GPIO_Driver.h \
- C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/inc/Stm32f103x6.h
-
-../Stm32_F103C6_Drivers/GPIO_DRIVER/Stm32F103C6_GPIO_Driver.h:
-
-C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/inc/Stm32f103x6.h:
