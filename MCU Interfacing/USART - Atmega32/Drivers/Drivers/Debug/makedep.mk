ae70100b2d62609f54e6e70cdb6df0451506e5b8
Belal Hani Sabha <137720072+belalsabha@users.noreply.github.com>
2025-12-21 14:01:09 +0200

    MCU Interfacing

---
+++ MCU Interfacing/USART - Atmega32/Drivers/Drivers/Debug/makedep.mk
@@ -0,0 +1,16 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+HAL\7Segment_DRIVER\Seven_Segment.c
+
+HAL\LCD_DRIVER\Lcd.c
+
+main.c
+
+MCAL\EXTI\EXTI.c
+
+MCAL\IO_PORTS\IO.c
+
+MCAL\USART\USART.c
+
