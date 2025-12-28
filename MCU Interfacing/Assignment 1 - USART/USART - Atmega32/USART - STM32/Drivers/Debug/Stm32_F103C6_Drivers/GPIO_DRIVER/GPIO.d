c2bb6d5c09eab4a1fc207c9894e3c26f768610b8
Belal Hani Sabha <137720072+belalsabha@users.noreply.github.com>
2025-12-28 12:43:38 +0200

    Delete MCU Interfacing/Assignment 1 - USART/USART - Atmega32/USART - STM32 directory

--- MCU Interfacing/Assignment 1 - USART/USART - Atmega32/USART - STM32/Drivers/Debug/Stm32_F103C6_Drivers/GPIO_DRIVER/GPIO.d
+++
@@ -1,8 +0,0 @@
-Stm32_F103C6_Drivers/GPIO_DRIVER/GPIO.o: \
- ../Stm32_F103C6_Drivers/GPIO_DRIVER/GPIO.c \
- ../Stm32_F103C6_Drivers/GPIO_DRIVER/GPIO.h \
- C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/inc/Stm32f103x6.h
-
-../Stm32_F103C6_Drivers/GPIO_DRIVER/GPIO.h:
-
-C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/inc/Stm32f103x6.h:
