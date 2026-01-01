3bba8497aca54a176fab727c0ed46903522220c2
Belal Hani Sabha <137720072+belalsabha@users.noreply.github.com>
2026-01-01 22:32:54 +0200

    MCU Interfacing

---
+++ MCU Interfacing/Assignment 3 - SPI/SPI - STM32/Drivers/Debug/Stm32_F103C6_Drivers/MCAL/SPI_DRIVER/SPI.d
@@ -0,0 +1,17 @@
+Stm32_F103C6_Drivers/MCAL/SPI_DRIVER/SPI.o: \
+ ../Stm32_F103C6_Drivers/MCAL/SPI_DRIVER/SPI.c \
+ ../Stm32_F103C6_Drivers/MCAL/SPI_DRIVER/SPI.h \
+ C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x6.h \
+ C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/inc/PLATFORM_TYPES.h \
+ C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/MCAL/GPIO_DRIVER/GPIO.h \
+ C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/MCAL/RCC_DRIVER/RCC.h
+
+../Stm32_F103C6_Drivers/MCAL/SPI_DRIVER/SPI.h:
+
+C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x6.h:
+
+C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/inc/PLATFORM_TYPES.h:
+
+C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/MCAL/GPIO_DRIVER/GPIO.h:
+
+C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/MCAL/RCC_DRIVER/RCC.h:
---
+++ MCU Interfacing/Assignment 3 - SPI/SPI - STM32/Drivers/Src/main.c
@@ -0,0 +1,20 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : Eng - Belal Hani Sabha
+ * @brief          : Application entry point
+ ******************************************************************************
+ */
+
+
+
+int main(void)
+{
+	/* Initialize Application */
+	APP_Init();
+
+	while (1)
+	{
+		APP_Start();
+	}
+}
