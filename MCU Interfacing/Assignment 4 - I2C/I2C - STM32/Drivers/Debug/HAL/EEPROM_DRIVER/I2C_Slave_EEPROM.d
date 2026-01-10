3a23815963ef0c15c16d642faa461dcf847a112d
Belal Hani Sabha <137720072+belalsabha@users.noreply.github.com>
2026-01-10 19:31:37 +0200

    MCU Interfacing

---
+++ MCU Interfacing/Assignment 4 - I2C/I2C - STM32/Drivers/Debug/HAL/EEPROM_DRIVER/I2C_Slave_EEPROM.d
@@ -0,0 +1,17 @@
+HAL/EEPROM_DRIVER/I2C_Slave_EEPROM.o: \
+ ../HAL/EEPROM_DRIVER/I2C_Slave_EEPROM.c \
+ ../HAL/EEPROM_DRIVER/I2C_Slave_EEPROM.h \
+ C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/MCAL/I2C_DRIVER/I2C.h \
+ C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x6.h \
+ C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/inc/PLATFORM_TYPES.h \
+ C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/MCAL/GPIO_DRIVER/GPIO.h
+
+../HAL/EEPROM_DRIVER/I2C_Slave_EEPROM.h:
+
+C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/MCAL/I2C_DRIVER/I2C.h:
+
+C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x6.h:
+
+C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/inc/PLATFORM_TYPES.h:
+
+C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/MCAL/GPIO_DRIVER/GPIO.h:
---
+++ MCU Interfacing/Assignment 4 - I2C/I2C - STM32/Drivers/Debug/HAL/EEPROM_DRIVER/subdir.mk
@@ -0,0 +1,19 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../HAL/EEPROM_DRIVER/I2C_Slave_EEPROM.c 
+
+OBJS += \
+./HAL/EEPROM_DRIVER/I2C_Slave_EEPROM.o 
+
+C_DEPS += \
+./HAL/EEPROM_DRIVER/I2C_Slave_EEPROM.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+HAL/EEPROM_DRIVER/I2C_Slave_EEPROM.o: ../HAL/EEPROM_DRIVER/I2C_Slave_EEPROM.c
+	arm-none-eabi-gcc  -gdwarf-2 "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DSTM32 -DSTM32F1 -DSTM32F103C6Tx -DDEBUG -c -I"C:/Users/Belal/Desktop/Drivers/APP" -I"C:/Users/Belal/Desktop/Drivers/HAL/EEPROM_DRIVER" -I"C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/MCAL/I2C_DRIVER" -I"C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/MCAL/SPI_DRIVER" -I"C:/Users/Belal/Desktop/Drivers/HAL/KEYPAD_DRIVER" -I"C:/Users/Belal/Desktop/Drivers/HAL/LCD_DRIVER" -I"C:/Users/Belal/Desktop/Drivers/HAL/Seven_Segment_DRIVER" -I"C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/inc" -I"C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/MCAL/EXTI_DRIVER" -I"C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/MCAL/GPIO_DRIVER" -I"C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/MCAL/RCC_DRIVER" -I"C:/Users/Belal/Desktop/Drivers/Stm32_F103C6_Drivers/MCAL/USART_DRIVER" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"HAL/EEPROM_DRIVER/I2C_Slave_EEPROM.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
