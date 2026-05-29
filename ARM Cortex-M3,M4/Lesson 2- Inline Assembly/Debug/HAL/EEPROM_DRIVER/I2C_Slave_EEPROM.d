579e4406d2ba1da884e327a9c6483ff3ebb0a262
Belal Hani Sabha <137720072+belalsabha@users.noreply.github.com>
2026-05-29 23:00:57 +0300

    ARM Cortex-M3,M4

---
+++ ARM Cortex-M3,M4/Lesson 2- Inline Assembly/Debug/HAL/EEPROM_DRIVER/I2C_Slave_EEPROM.d
@@ -0,0 +1,17 @@
+HAL/EEPROM_DRIVER/I2C_Slave_EEPROM.o: \
+ ../HAL/EEPROM_DRIVER/I2C_Slave_EEPROM.c \
+ ../HAL/EEPROM_DRIVER/I2C_Slave_EEPROM.h \
+ D:/Embedded\ System/GitHub/Master_Embedded_Systems/MCU\ Interfacing/Assignment\ 2\ -\ Update\ USART/USART\ -\ STM32/Drivers/Stm32_F103C6_Drivers/MCAL/I2C_DRIVER/I2C.h \
+ D:/Embedded\ System/GitHub/Master_Embedded_Systems/MCU\ Interfacing/Assignment\ 2\ -\ Update\ USART/USART\ -\ STM32/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x6.h \
+ D:/Embedded\ System/GitHub/Master_Embedded_Systems/MCU\ Interfacing/Assignment\ 2\ -\ Update\ USART/USART\ -\ STM32/Drivers/Stm32_F103C6_Drivers/inc/PLATFORM_TYPES.h \
+ D:/Embedded\ System/GitHub/Master_Embedded_Systems/MCU\ Interfacing/Assignment\ 2\ -\ Update\ USART/USART\ -\ STM32/Drivers/Stm32_F103C6_Drivers/MCAL/GPIO_DRIVER/GPIO.h
+
+../HAL/EEPROM_DRIVER/I2C_Slave_EEPROM.h:
+
+D:/Embedded\ System/GitHub/Master_Embedded_Systems/MCU\ Interfacing/Assignment\ 2\ -\ Update\ USART/USART\ -\ STM32/Drivers/Stm32_F103C6_Drivers/MCAL/I2C_DRIVER/I2C.h:
+
+D:/Embedded\ System/GitHub/Master_Embedded_Systems/MCU\ Interfacing/Assignment\ 2\ -\ Update\ USART/USART\ -\ STM32/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x6.h:
+
+D:/Embedded\ System/GitHub/Master_Embedded_Systems/MCU\ Interfacing/Assignment\ 2\ -\ Update\ USART/USART\ -\ STM32/Drivers/Stm32_F103C6_Drivers/inc/PLATFORM_TYPES.h:
+
+D:/Embedded\ System/GitHub/Master_Embedded_Systems/MCU\ Interfacing/Assignment\ 2\ -\ Update\ USART/USART\ -\ STM32/Drivers/Stm32_F103C6_Drivers/MCAL/GPIO_DRIVER/GPIO.h:
